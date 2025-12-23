a8411ded1f8539d760059b2fec1d2b5ba78f6fbb
bluuewhale <koko8624@gmail.com>
2025-12-24 00:21:35 +0900

    refactor: enhance ConcurrentSwissMap with additional ConcurrentMap methods

--- src/apacheTest/java/io/github/bluuewhale/hashsmith/ApacheConcurrentSwissMapTest.java
+++ src/apacheTest/java/io/github/bluuewhale/hashsmith/ApacheConcurrentSwissMapTest.java
@@ -5,16 +5,8 @@ import org.apache.commons.collections4.map.AbstractMapTest;
 import java.util.Map;
 
 final class ApacheConcurrentSwissMapTest<K, V> extends AbstractMapTest<Map<K, V>, K, V> {
-    @Override public boolean isAllowNullKey() {
-        return false;
-    }
-    @Override public boolean isAllowNullValueGet() {
-        return true;
-    }
-    @Override public boolean isAllowNullValuePut() {
-        return true;
-    }
-    @Override public Map<K, V> makeObject() {
-        return new ConcurrentSwissMap<>();
-    }
+    @Override public boolean isAllowNullKey() { return false; }
+    @Override public boolean isAllowNullValueGet() { return true; }
+    @Override public boolean isAllowNullValuePut() { return true; }
+    @Override public Map<K, V> makeObject() { return new ConcurrentSwissMap<>(); }
 }
